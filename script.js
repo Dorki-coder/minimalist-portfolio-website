48f6eb64f181a9f6977552ef1aceac2f6e4c9c33
Dorki <bartdane@yandex.ru>
2022-10-14 19:34:46 +0600

    -add propogination -fix addaptive

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const burger = document.querySelector(".burger-button");
 const navMenu = document.querySelector(".burger-nav");
 
-burger.addEventListener("click", () => {
-  console.log("click");
+burger.addEventListener("click", function (e) {
+  e.preventDefault();
+  e.stopPropagation();
   burger.classList.toggle("active");
   navMenu.classList.toggle("active");
+});
+
+document.addEventListener("click", () => {
+  burger.classList.remove("active");
+  navMenu.classList.remove("active");
 });
